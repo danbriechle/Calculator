84681d6fbaaeb728054d7cff8b47757d9d30293d
Daniel Briechle <danbriechle@hotmail.com>
2019-04-07 20:05:34 -0600

    Adds root route

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 RSpec.describe "root", type: :feature do
   it 'can see a form for calculations' do
     visit '/'
-  expect(page).to have_button("Calculate")
 
+    expect(page).to have_button("Calculate")
   end
 end
