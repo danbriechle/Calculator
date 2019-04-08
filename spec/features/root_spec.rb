8196b012578409776bb881b2d1703405d847d52d
Daniel Briechle <danbriechle@hotmail.com>
2019-04-07 21:42:44 -0600

    Adds testing per spec

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -14,9 +14,25 @@ RSpec.describe "root", type: :feature do
 
     click_button 'Calculate'
 
-    save_and_open_page
-
     expect(page).to have_content("result: 15")
     expect(page).to have_content('3*5')
+  end
+
+  it 'can get the answer to complex problems' do
+    visit '/'
+
+    fill_in :calculator_input, with: "5*3+1+6/2+9*100"
+
+    click_button 'Calculate'
+
+    expect(page).to have_content("result: 919")
+    expect(page).to have_content("5*3+1+6/2+9*100")
+
+    fill_in :calculator_input, with: "5*3+1+6/85+9*100"
+
+    click_button 'Calculate'
+
+    expect(page).to have_content("result: 916.07")
+    expect(page).to have_content("5*3+1+6/85+9*100")
   end
 end
