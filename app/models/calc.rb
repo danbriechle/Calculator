e67caf65057f0ee8bb7589e677705884e239455d
Daniel Briechle <danbriechle@hotmail.com>
2019-04-07 21:02:47 -0600

    Adds initial divide functionality to calculate method

--- app/models/calc.rb
+++ app/models/calc.rb
@@ -1,9 +1,16 @@
 class Calc
   def self.calculate(math_string)
   result = 0
-   math_string.scan(/(\d*)([*])(\d*)/).each do |array|
-      result += array[0].to_i * array[2].to_i
-    end
+  # multiply
+    math_string.scan(/(\d*)([*])(\d*)/).each do |array|
+        result += array[0].to_i * array[2].to_i
+     end
+
+  #divide
+    math_string.scan(/(\d*)([\/])(\d*)/).each do |array|
+      result += array[0].to_i / array[2].to_i
+      end
+
   result
   end
 end
--- spec/poros/calc_spec.rb
+++ spec/poros/calc_spec.rb
@@ -3,9 +3,10 @@ RSpec.describe Calc, type: :poro do
     describe ".calculate" do
       it "can preform single calculations" do
       input = '5*3'
-    
+      input_2 = '6/2'
 
       expect(Calc.calculate(input)).to eq(15)
+      expect(Calc.calculate(input_2)).to eq(3)
       end
     end
   end
