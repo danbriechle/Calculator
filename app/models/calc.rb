3ff87a3b356ecf02e8b7ded7f640fedf8f874916
Daniel Briechle <danbriechle@hotmail.com>
2019-04-07 21:11:01 -0600

    Adds initial multiple calculation functionality

--- app/models/calc.rb
+++ app/models/calc.rb
@@ -4,20 +4,24 @@ class Calc
   # multiply
     math_string.scan(/(\d*)([*])(\d*)/).each do |array|
         result += array[0].to_i * array[2].to_i
+        math_string.sub!(array.join,"")
      end
 
   #divide
     math_string.scan(/(\d*)([\/])(\d*)/).each do |array|
       result += array[0].to_i / array[2].to_i
+      math_string.sub!(array.join,"")
       end
 
   #add
     math_string.scan(/(\d*)([+])(\d*)/).each do |array|
       result += array[0].to_i + array[2].to_i
+      math_string.sub!(array.join,"")
       end
   #subtract
     math_string.scan(/(\d*)([-])(\d*)/).each do |array|
       result += array[0].to_i - array[2].to_i
+      math_string.sub!(array.join,"")
       end
 
   result
